d3fad3286e0372500f4e4a3f36a7bc15672b67b4
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2019-03-23 20:09:47 -0300

    add: array and slices

---
+++ arrays/arrays.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	lista1 := [2]int{1, 2}
+	i := 0
+	for range lista1 {
+		fmt.Println(lista1[i])
+		i++
+
+	}
+
+	fmt.Println()
+	slice1 := []string{"a", "b"}
+	for i := 0; i < len(slice1); i++ {
+		fmt.Println(slice1[i])
+	}
+
+	fmt.Println()
+	slice2 := make([]int, 2)
+	fmt.Println(len(slice2))
+
+	fmt.Println()
+	var slice3 []int
+	fmt.Println(len(slice3))
+
+	for i := 0; i < 10; i++ {
+		slice3 = append(slice3, i)
+	}
+	fmt.Println("slice3:", slice3)
+}
