a3625a0e4e75b0a17d2aee154bb9da24ad759bd9
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2019-03-24 11:45:57 -0300

    add: methods

--- structs/structs.go
+++ structs/structs.go
@@ -7,6 +7,14 @@ type pessoa struct {
 	idade int
 }
 
+func (p pessoa) getNome() string {
+	return p.nome
+}
+
+func (p *pessoa) getIdade(adicional int) int {
+	return p.idade + adicional
+}
+
 func main() {
 	p1 := pessoa{"fabio", 10}
 	fmt.Println(p1)
@@ -17,4 +25,7 @@ func main() {
 
 	fmt.Println(p1)
 	fmt.Println(p2)
+
+	fmt.Println("Nome via metodo:", p2.getNome())
+	fmt.Println("idade via metodo:", p2.getIdade(2))
 }
